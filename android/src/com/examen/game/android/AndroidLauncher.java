aaf54dad06750e5fda253a387a987b9244d8bfd2
rasantos24 <rafa.santos.24@unitec.edu>
2014-11-14 08:18:56 -0600

    Examen

---
+++ android/src/com/examen/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.examen.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.examen.game.ExamenLab1;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ExamenLab1(), config);
+	}
+}
---
+++ desktop/src/com/examen/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.examen.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.examen.game.ExamenLab1;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ExamenLab1(), config);
+	}
+}
